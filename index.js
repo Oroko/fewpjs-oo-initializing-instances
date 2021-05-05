329229491cc5d25c7b6b1cc2e5699037bd2efa81
Oroko <aoroko09@gmail.com>
2021-05-05 14:51:40 -0400

    commit tree 09dd1a7a6f302e27e75353cd56ecb610cb44ee5a

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your code here
+
+class Breakfast {
+  constructor(food,drink){
+    this.food = food
+    this.drink = drink
+  }
+}
+
+let breko = new Breakfast('toast','coffee')
+
+class Lunch{
+  constructor(salad, soup, drink){
+    this.salad = salad
+    this.soup = soup
+    this.drink = drink
+  }
+}
+
+let lunch = new Lunch('Corn salad', 'tomato','water')
+
+class Dinner {
+  constructor(salad, soup, entree, dessert){
+    this.salad = salad 
+    this.soup = soup
+    this.entree = entree 
+    this._dessert = dessert
+  }
+}
+let dinner = new Dinner("kale salad", "pea soup", "wings", "cheesecake");
